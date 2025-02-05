74555e86c78c04731717d7c5b12ea005bb8be29d
Codeliu <lzlalpha@gmail.com>
2025-02-05 17:27:11 +0800

    Update aiListText.vue

--- src/views/mj/aiListText.vue
+++ src/views/mj/aiListText.vue
@@ -9,7 +9,7 @@ defineProps<{myItem:Chat.History,myObj?:gptConfigType}>()
 <span class="flex justify-start items-center">
 
     <SvgIcon icon="ri:message-3-line"   v-if="!myObj  "  />
-    <n-avatar v-else-if="myObj.gpts" :src="myObj.gpts.logo" fallback-src="../../assets/avatar.jpg" :size="18" round/> 
+    <n-avatar v-else-if="myObj.gpts" :src="myObj.gpts.logo" fallback-src="../../assets/robot.png" :size="18" round/> 
     <SvgIcon icon="bi:chat"  v-else/>
 </span>
 <div class="relative flex-1 overflow-hidden break-all text-ellipsis whitespace-nowrap">
@@ -22,7 +22,7 @@ defineProps<{myItem:Chat.History,myObj?:gptConfigType}>()
         <ul  > 
         <template v-if="myObj.gpts">
             <li class="flex justify-start items-center space-x-2">
-            <n-avatar :src="myObj.gpts.logo" fallback-src="../../assets/avatar.jpg" round/> 
+            <n-avatar :src="myObj.gpts.logo" fallback-src="../../assets/robot.png" round/> 
             <span >{{ myObj.gpts.name }}</span>
             </li>
             <li >ID: {{myObj.model }}</li>
