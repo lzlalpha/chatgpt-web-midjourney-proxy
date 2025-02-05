49e01fe546270e407152e53812d1feef18071e59
Codeliu <lzlalpha@gmail.com>
2025-02-05 17:02:05 +0800

    Update index.vue

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -72,7 +72,6 @@ const show = computed({
         <NTabPane name="Config" tab="Config">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <!-- <span class="ml-2">{{ $t('setting.config') }}</span> -->
             <span class="ml-2">{{ $t('mjset.about') }}</span>
           </template>
           <About />
